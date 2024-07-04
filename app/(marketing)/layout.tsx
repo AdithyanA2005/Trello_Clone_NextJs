915991d25a19584e583e68122429784a395479d1
Adithyan A <deepaadithyan56@gmail.com>
2024-07-04 15:36:40 +0530

    add marketing page with dedicated navbar and footer

---
+++ app/(marketing)/layout.tsx
@@ -0,0 +1,13 @@
+import React from "react";
+import { Footer } from "@/app/(marketing)/_components/footer";
+import { Navbar } from "./_components/navbar";
+
+export default function MarketingLayout({ children }: { children: React.ReactNode }) {
+  return (
+    <div className="h-full bg-slate-100">
+      <Navbar />
+      <main className="bg-stone-100 pb-20 pt-40">{children}</main>
+      <Footer />
+    </div>
+  );
+}
---
+++ app/(marketing)/page.tsx
@@ -0,0 +1,46 @@
+import { Poppins } from "next/font/google";
+import localFont from "next/font/local";
+import Link from "next/link";
+import { MedalIcon } from "lucide-react";
+import { Button } from "@/components/ui/button";
+import { siteConfig } from "@/config/site";
+import { cn } from "@/lib/utils";
+
+const headingFont = localFont({
+  src: "../../public/fonts/cal-sans.woff2",
+});
+
+const textFont = Poppins({
+  subsets: ["latin"],
+  weight: ["100", "200", "300", "400", "500", "600", "700", "800", "900"],
+});
+
+export default function MarketingPage() {
+  return (
+    <div className="flex flex-col items-center justify-center">
+      <div className={cn("flex flex-col items-center justify-center", headingFont.className)}>
+        <div className="mb-4 flex items-center rounded-full border bg-amber-100 p-4 uppercase text-amber-700 shadow-sm">
+          <MedalIcon className="mr-2 size-6" />
+          No 1 task management
+        </div>
+        <h1 className="mb-6 text-center text-3xl text-neutral-800 md:text-6xl">{siteConfig.name} helps teams move</h1>
+        <div className="w-fit rounded-md bg-gradient-to-r from-fuchsia-600 to-pink-600 p-2 px-4 pb-4 text-3xl text-white md:text-6xl">
+          Work forward
+        </div>
+      </div>
+
+      <div
+        className={cn(
+          "mx-auto mt-4 max-w-xs text-center text-sm text-neutral-400 md:max-w-2xl md:text-xl",
+          textFont.className,
+        )}
+      >
+        {siteConfig.description}
+      </div>
+
+      <Button className="mt-6" size="lg" asChild>
+        <Link href="/sign-up">Get {siteConfig.name} for free</Link>
+      </Button>
+    </div>
+  );
+}

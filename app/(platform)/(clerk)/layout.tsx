c8e70af6c4abffafeb8fa002a1c4b9d9b6ae4090
Adithyan A <deepaadithyan56@gmail.com>
2024-07-04 21:10:29 +0530

    add semantic main tag to (clerk)/layout.tsx

--- app/(platform)/(clerk)/layout.tsx
+++ app/(platform)/(clerk)/layout.tsx
@@ -1,5 +1,5 @@
 import React from "react";
 
 export default function ClerkLayout({ children }: { children: React.ReactNode }) {
-  return <div className="flex h-full items-center justify-center">{children}</div>;
+  return <main className="flex h-full items-center justify-center">{children}</main>;
 }

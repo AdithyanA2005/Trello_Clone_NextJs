f1764af9c313fb3e182b710351be0e653d18f21b
Adithyan A <deepaadithyan56@gmail.com>
2024-07-04 15:53:49 +0530

    App name changed from Taskify -> AdisTaskify

--- config/site.ts
+++ config/site.ts
@@ -1,5 +1,5 @@
 export const siteConfig = {
-  name: "Taskify",
+  name: "AdisTaskify",
   description:
-    "Collaborate, manage projects, and reach new productivity peaks. From high rises to the home office, the way your team works is unique - accomplish it all with Taskify.",
+    "Collaborate, manage projects, and reach new productivity peaks. From high rises to the home office, the way your team works is unique - accomplish it all with AdisTaskify.",
 };

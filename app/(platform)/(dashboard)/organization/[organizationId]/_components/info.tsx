b059fab47dfd6fd6fa7030d2a0d7faa00804dcc8
Adithyan A <deepaadithyan56@gmail.com>
2024-07-07 00:03:16 +0530

    create an <Info /> which shows org title in organization page.

---
+++ app/(platform)/(dashboard)/organization/[organizationId]/_components/info.tsx
@@ -0,0 +1,45 @@
+"use client";
+
+import Image from "next/image";
+import { useOrganization } from "@clerk/nextjs";
+import { CreditCardIcon } from "lucide-react";
+import { Skeleton } from "@/components/ui/skeleton";
+
+export function Info() {
+  const { organization, isLoaded } = useOrganization();
+
+  if (!isLoaded) return <Info.Skeleton />;
+
+  return (
+    <div className="flex items-center gap-x-4">
+      <div className="relative h-[60px] w-[60px]">
+        <Image fill src={organization?.imageUrl!} alt="organization" className="rounded-md object-cover" />
+      </div>
+
+      <div className="space-y-1">
+        <p className="text-xl font-semibold">{organization?.name}</p>
+        <div className="flex items-center text-xs text-muted-foreground">
+          <CreditCardIcon className="mr-1 size-3" />
+          Free
+        </div>
+      </div>
+    </div>
+  );
+}
+
+Info.Skeleton = function InfoSkeleton() {
+  return (
+    <div className="flex items-center gap-x-4">
+      <div className="h-[60px] w-[60px]">
+        <Skeleton className="h-full w-full" />
+      </div>
+      <div className="space-y-2">
+        <Skeleton className="h-9 w-[200px]" />
+        <div className="flex items-center">
+          <Skeleton className="mr-2 h-4 w-4" />
+          <Skeleton className="h-4 w-[100px]" />
+        </div>
+      </div>
+    </div>
+  );
+};
--- app/(platform)/(dashboard)/organization/[organizationId]/page.tsx
+++ app/(platform)/(dashboard)/organization/[organizationId]/page.tsx
@@ -1,3 +1,11 @@
-export default function OrganizationPage() {
-  return <div>OrganizationPage</div>;
+import { Separator } from "@/components/ui/separator";
+import { Info } from "./_components/info";
+
+export default async function OrganizationPage() {
+  return (
+    <div className="mb-20 w-full">
+      <Info />
+      <Separator className="my-4" />
+    </div>
+  );
 }

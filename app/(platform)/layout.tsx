382c0b64dc0cdc739edee9b7b0f3aa07277bead4
Adithyan A <deepaadithyan56@gmail.com>
2024-07-06 11:28:58 +0530

    install `sonner` package & Add `<Toaster />` component to `(platform)/layout.tsx`

--- app/(platform)/layout.tsx
+++ app/(platform)/layout.tsx
@@ -1,6 +1,12 @@
 import React from "react";
 import { ClerkProvider } from "@clerk/nextjs";
+import { Toaster } from "sonner";
 
 export default function PlatFormLayout({ children }: { children: React.ReactNode }) {
-  return <ClerkProvider>{children}</ClerkProvider>;
+  return (
+    <ClerkProvider>
+      {children}
+      <Toaster />
+    </ClerkProvider>
+  );
 }

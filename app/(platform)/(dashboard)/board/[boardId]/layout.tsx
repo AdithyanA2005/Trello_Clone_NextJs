2319b2bf3fe7b4bb63c7a8069a16682964367cb5
Adithyan A <deepaadithyan56@gmail.com>
2024-07-07 14:06:44 +0530

    create layout.tsx for board/{id} with auto generated metadata

---
+++ app/(platform)/(dashboard)/board/[boardId]/layout.tsx
@@ -0,0 +1,46 @@
+import React from "react";
+import { notFound, redirect } from "next/navigation";
+import { auth } from "@clerk/nextjs/server";
+import { prisma } from "@/lib/db";
+
+interface IParams {
+  boardId: string;
+}
+
+export async function generateMetadata({ params }: { params: IParams }) {
+  const { orgId } = auth();
+  if (!orgId) return { title: "Board" };
+
+  const board = await prisma.board.findUnique({
+    where: {
+      id: params.boardId,
+      orgId,
+    },
+  });
+  return { title: board?.title || "Board" };
+}
+
+export default async function BoardIdLayout({ children, params }: { children: React.ReactNode; params: IParams }) {
+  const { orgId } = auth();
+  if (!orgId) redirect("/select-org");
+
+  const board = await prisma.board.findUnique({
+    where: {
+      id: params.boardId,
+      orgId,
+    },
+  });
+  if (!board) notFound();
+
+  const { boardId } = params;
+
+  return (
+    <div
+      style={{ backgroundImage: `url(${board.imageFullUrl})` }}
+      className="relative h-full bg-cover bg-center bg-no-repeat"
+    >
+      <div className="absolute inset-0 bg-black/10" />
+      <main className="relative h-full pt-28">{children}</main>
+    </div>
+  );
+}

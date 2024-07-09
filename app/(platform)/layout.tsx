47ee800ff20d170df1ec3f0205913eeb07d737b9
Adithyan A <deepaadithyan56@gmail.com>
2024-07-09 21:48:47 +0530

    create GET route handler to fetch a card

--- app/(platform)/layout.tsx
+++ app/(platform)/layout.tsx
@@ -1,15 +1,16 @@
 import React from "react";
 import { ClerkProvider } from "@clerk/nextjs";
 import { Toaster } from "sonner";
+import { ModalProvider } from "@/components/providers/modal-provider";
 import { QueryProvider } from "@/components/providers/query-provider";
 
 export default function PlatFormLayout({ children }: { children: React.ReactNode }) {
   return (
     <ClerkProvider>
-      {children}
-      <Toaster />
       <QueryProvider>
         {children}
+
+        <ModalProvider />
         <Toaster />
       </QueryProvider>
     </ClerkProvider>
---
+++ app/api/cards/[cardId]/route.ts
@@ -0,0 +1,35 @@
+import { NextResponse } from "next/server";
+import { auth } from "@clerk/nextjs/server";
+import { prisma } from "@/lib/db";
+
+/**
+ * GET handler for fetching card details by ID.
+ * - Authenticates user.
+ * - Fetches card details if user is authorized.
+ * - Returns card details or error response.
+ */
+export async function GET(req: Request, { params }: { params: { cardId: string } }) {
+  try {
+    // Authenticate user.
+    const { userId, orgId } = auth();
+    if (!userId || !orgId) return new NextResponse("Unauthorized", { status: 401 });
+
+    // Fetch card details.
+    const card = await prisma.card.findUnique({
+      where: {
+        id: params.cardId,
+        list: { board: { orgId } },
+      },
+      include: {
+        list: {
+          select: {
+            title: true,
+          },
+        },
+      },
+    });
+    return NextResponse.json(card);
+  } catch (error) {
+    return new NextResponse("Internal Error", { status: 500 });
+  }
+}

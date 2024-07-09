79b49d0b0a7c4de285f03537e3e646f0e885ccb6
Adithyan A <deepaadithyan56@gmail.com>
2024-07-09 21:41:43 +0530

    add query-provider.tsx for ``@tanstack/react-query`

--- app/(platform)/layout.tsx
+++ app/(platform)/layout.tsx
@@ -1,12 +1,17 @@
 import React from "react";
 import { ClerkProvider } from "@clerk/nextjs";
 import { Toaster } from "sonner";
+import { QueryProvider } from "@/components/providers/query-provider";
 
 export default function PlatFormLayout({ children }: { children: React.ReactNode }) {
   return (
     <ClerkProvider>
       {children}
       <Toaster />
+      <QueryProvider>
+        {children}
+        <Toaster />
+      </QueryProvider>
     </ClerkProvider>
   );
 }

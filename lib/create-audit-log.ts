facf73f224a90b85f8179d51dd5d5d2d667a8ff4
Adithyan A <deepaadithyan56@gmail.com>
2024-07-10 13:54:52 +0530

    create a helper function to create new audit log in db

---
+++ lib/create-audit-log.ts
@@ -0,0 +1,29 @@
+import { auth, currentUser } from "@clerk/nextjs/server";
+import { ACTION, ENTITY_TYPE } from "@prisma/client";
+import { prisma } from "@/lib/db";
+
+interface Props {
+  entityId: string;
+  entityType: ENTITY_TYPE;
+  entityTitle: string;
+  action: ACTION;
+}
+
+export async function createAuditLog({ entityId, entityType, entityTitle, action }: Props) {
+  const { orgId } = auth();
+  const user = await currentUser();
+  if (!user || !orgId) return;
+
+  await prisma.auditLog.create({
+    data: {
+      action,
+      entityType,
+      entityId,
+      entityTitle,
+      orgId,
+      userId: user.id,
+      userImage: user.imageUrl,
+      userName: user.firstName + " " + user.lastName,
+    },
+  });
+}

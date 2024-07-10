bfc1b2ee13dfaa3b8445960ccbbda332c8014e14
Adithyan A <deepaadithyan56@gmail.com>
2024-07-10 21:39:31 +0530

    create helper function to checkSubscription

---
+++ lib/subscription.ts
@@ -0,0 +1,25 @@
+import { auth } from "@clerk/nextjs/server";
+import { prisma } from "@/lib/db";
+
+const DAY_IN_MS = 1000 * 60 * 60 * 24;
+
+export async function checkSubscription() {
+  const { orgId } = auth();
+  if (!orgId) return false;
+
+  const orgSubscription = await prisma.orgSubscription.findUnique({
+    where: { orgId },
+    select: {
+      stripePriceId: true,
+      stripeCustomerId: true,
+      stripeSubscriptionId: true,
+      stripeCurrentPeriodEnd: true,
+    },
+  });
+  if (!orgSubscription) return false;
+
+  const isValid =
+    orgSubscription.stripePriceId && orgSubscription.stripeCurrentPeriodEnd?.getTime()! + DAY_IN_MS > Date.now();
+
+  return !!isValid;
+}

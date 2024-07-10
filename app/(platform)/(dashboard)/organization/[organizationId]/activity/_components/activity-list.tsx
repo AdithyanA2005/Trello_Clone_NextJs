e2dabfe216e23dd97fef337b276109e60f16410d
Adithyan A <deepaadithyan56@gmail.com>
2024-07-10 15:08:48 +0530

    create dedicated activity page for organizations

---
+++ app/(platform)/(dashboard)/organization/[organizationId]/activity/_components/activity-list.tsx
@@ -0,0 +1,38 @@
+import { redirect } from "next/navigation";
+import { auth } from "@clerk/nextjs/server";
+import { Skeleton } from "@/components/ui/skeleton";
+import { ActivityItem } from "@/components/activity-item";
+import { prisma } from "@/lib/db";
+
+export async function ActivityList() {
+  const { orgId } = auth();
+  if (!orgId) redirect("/select-org");
+
+  const auditLogs = await prisma.auditLog.findMany({
+    where: { orgId },
+    orderBy: { createdAt: "desc" },
+  });
+
+  return (
+    <ol className="mt-4 space-y-4">
+      <p className="hidden text-center text-xs text-muted-foreground last:block">
+        No activity found inside this organization
+      </p>
+      {auditLogs.map((log) => (
+        <ActivityItem data={log} key={log.id} />
+      ))}
+    </ol>
+  );
+}
+
+ActivityList.Skeleton = function ActivityListSkeleton() {
+  return (
+    <ol className="mt-4 space-y-4">
+      <Skeleton className="h-14 w-[80%]" />
+      <Skeleton className="h-14 w-[50%]" />
+      <Skeleton className="h-14 w-[70%]" />
+      <Skeleton className="h-14 w-[80%]" />
+      <Skeleton className="h-14 w-[75%]" />
+    </ol>
+  );
+};
---
+++ app/(platform)/(dashboard)/organization/[organizationId]/activity/page.tsx
@@ -0,0 +1,17 @@
+import { Suspense } from "react";
+import { Separator } from "@/components/ui/separator";
+import { ActivityList } from "@/app/(platform)/(dashboard)/organization/[organizationId]/activity/_components/activity-list";
+import { Info } from "../_components/info";
+
+export default function ActivityPage() {
+  return (
+    <div className="w-full">
+      <Info />
+
+      <Separator className="my-2" />
+      <Suspense fallback={<ActivityList.Skeleton />}>
+        <ActivityList />
+      </Suspense>
+    </div>
+  );
+}

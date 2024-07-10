7060672ba222e2d949a9c31323bf52349b6c4cbf
Adithyan A <deepaadithyan56@gmail.com>
2024-07-10 13:43:27 +0530

    create a helper function to generate audit log message

---
+++ lib/generate-log-message.ts
@@ -0,0 +1,14 @@
+import { ACTION, AuditLog } from "@prisma/client";
+
+export function generateLogMessage({ action, entityTitle, entityType }: AuditLog) {
+  switch (action) {
+    case ACTION.CREATE:
+      return `created ${entityType.toLowerCase()} "${entityTitle}"`;
+    case ACTION.UPDATE:
+      return `updated ${entityType.toLowerCase()} "${entityTitle}"`;
+    case ACTION.DELETE:
+      return `deleted ${entityType.toLowerCase()} "${entityTitle}"`;
+    default:
+      return `unknown action ${entityType.toLowerCase()} "${entityTitle}"`;
+  }
+}

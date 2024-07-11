48f353b954e8351fc0102c31eda79b6000f6166b
Adithyan A <deepaadithyan56@gmail.com>
2024-07-11 15:18:18 +0530

    Add more semantic tags and change heading color in sidebar

--- app/(platform)/(dashboard)/_components/sidebar.tsx
+++ app/(platform)/(dashboard)/_components/sidebar.tsx
@@ -6,6 +6,7 @@ import { PlusIcon } from "lucide-react";
 import { useLocalStorage } from "usehooks-ts";
 import { Accordion } from "@/components/ui/accordion";
 import { Button } from "@/components/ui/button";
+import { Separator } from "@/components/ui/separator";
 import { Skeleton } from "@/components/ui/skeleton";
 import { NavItem, Organization } from "./nav-item";
 
@@ -30,45 +31,53 @@ export function Sidebar() {
     setExpanded((prev) => ({ ...prev, [id]: !expanded[id] }));
   };
 
-  if (isLoading) {
-    return (
-      <aside>
-        <div className="mb-1 flex h-[40px] items-center">
-          <Skeleton className="ml-4 h-7 w-[50%]" />
-          <Skeleton className="ml-auto size-10" />
-        </div>
-
-        <div className="space-y-2">
-          <NavItem.Skeleton />
-          <NavItem.Skeleton />
-          <NavItem.Skeleton />
-        </div>
-      </aside>
-    );
-  }
+  if (isLoading) return <Sidebar.Skeleton />;
 
   return (
     <aside>
-      <div className="mb-1 flex items-center text-xs font-medium">
-        <span className="pl-4">Workspaces</span>
-        <Button asChild size="icon" type="button" variant="ghost" className="ml-auto">
-          <Link href="/select-org">
-            <PlusIcon className="size-4" />
-          </Link>
-        </Button>
-      </div>
+      <section aria-labelledby="workspaces-header">
+        <div className="flex h-[40px] items-center text-gray-800">
+          <h2 id="workspaces-header" className="pl-4 text-xs font-semibold">
+            Workspaces
+          </h2>
+          <Button asChild size="icon" type="button" variant="ghost" className="ml-auto size-8">
+            <Link href="/select-org">
+              <PlusIcon className="size-4" />
+            </Link>
+          </Button>
+        </div>
 
-      <Accordion type="multiple" defaultValue={defaultAccordionValue} className="space-y-2">
-        {userMemberships.data?.map(({ organization }) => (
-          <NavItem
-            key={organization.id}
-            isActive={activeOrg?.id === organization.id}
-            isExpanded={expanded[organization.id]}
-            organization={organization as Organization}
-            onExpand={toggleAccordionItem}
-          />
-        ))}
-      </Accordion>
+        <Accordion type="multiple" defaultValue={defaultAccordionValue} className="space-y-2">
+          <nav>
+            {userMemberships.data?.map(({ organization }) => (
+              <NavItem
+                key={organization.id}
+                isActive={activeOrg?.id === organization.id}
+                isExpanded={expanded[organization.id]}
+                organization={organization as Organization}
+                onExpand={toggleAccordionItem}
+              />
+            ))}
+          </nav>
+        </Accordion>
+      </section>
     </aside>
   );
 }
+
+Sidebar.Skeleton = function SidebarSkeleton() {
+  return (
+    <aside>
+      <div className="flex h-[40px] items-center">
+        <Skeleton className="ml-4 h-6 w-[50%]" />
+        <Skeleton className="ml-auto size-8" />
+      </div>
+
+      <div className="space-y-2">
+        <NavItem.Skeleton />
+        <NavItem.Skeleton />
+        <NavItem.Skeleton />
+      </div>
+    </aside>
+  );
+};
